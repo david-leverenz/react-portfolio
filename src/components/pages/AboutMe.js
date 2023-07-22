3f65a0f87bc7771fb5e6f66a71ab677bb238e926
david-leverenz <david.leverenz@gmail.com>
2023-07-22 12:33:36 -0500

    overlay working and app descriptions done

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -15,7 +15,7 @@ const AboutMe = () => {
                                 <div className="pt-3">
                                     <p className="text-center p-3 m-2 bg-secondary rounded text-light">
 
-                                        I've worked with and around developers my entire career which includes management of professional services, consulting, product management, technical support, content management and some sales.  What our development teams were able to do with code always fascinated me.  As a result I did a lot of work in MS Access because I was able to build applications without needing knowledge of coding, per se.  Recently I decided to take a coding class to learn how to do things the right way.  Below are a few of the things I built in class.  I hope you like them.
+                                        I've worked with and around developers my entire career which includes management of professional services, consulting, product management, technical support, content management and some sales.  What our development teams were able to do with code always fascinated me.  As a result I did a lot of work in MS Access because I was able to build applications without needing knowledge of coding, per se.  Recently I decided to take a coding class to learn how to do things the right way.  Checkout my Portfolio page to see a few of the things I created in class.  I hope you like them.
                                     </p>
                                 </div>
 
