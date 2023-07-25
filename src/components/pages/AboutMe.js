2ea2b89c31cc70aa6f0557408ba2b113c816e48d
david-leverenz <david.leverenz@gmail.com>
2023-07-25 09:11:27 -0500

    removed space from a file

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -15,7 +15,6 @@ const AboutMe = ({ handlepagechange }) => {
                             <div className="col-8">
                                 <div className="pt-3">
                                     <p className="text-center ps-5 pe-5 pb-3 pt-2 mt-2 bg-secondary rounded text-light fs-5 lh-lg">
-
                                         I've worked with and around developers my entire career which includes management of professional services, consulting, product management, technical support, content management and some sales.  What our development teams were able to do with code always fascinated me.  As a result I did a lot of work in MS Access because I was able to build applications without needing knowledge of coding, per se.  Recently I decided to take a coding class to learn how to do things the right way.  Checkout my Portfolio page to see a few of the things I created in class.  I hope you like them.
                                     </p>
                                 </div>
