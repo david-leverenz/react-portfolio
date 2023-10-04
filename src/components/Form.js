d6b8714530d6b456ece398664732c5cfbe46183e
david-leverenz <david.leverenz@gmail.com>
2023-10-04 09:33:24 -0500

    fixed submit button to trigger alert

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 
 // Helper function that will check if the email is valid.
-import { validateEmail } from '../utils/helpers';
+// import { validateEmail } from '../utils/helpers';
 import './style.css';
 
 function Form() {
+
+    const clickSubmit = () => {
+        // Display an alert when the link is clicked
+        window.alert('Feedback form is unavailable at this time, please email directly to david.leverenz@gmail.com.  Thank you.');
+      };
+
     // Create state variables for the fields in the form.
     // We are also setting their initial values to an empty string.
     const [name, setName] = useState('');
@@ -32,27 +38,27 @@ function Form() {
         }
     };
 
-    const handleFormSubmit = (e) => {
-        // Preventing the default behavior of the form submit (which is to refresh the page, which I think should be a default behavior.)
-        e.preventDefault();
+    // const handleFormSubmit = (e) => {
+    //     // Preventing the default behavior of the form submit (which is to refresh the page, which I think should be a default behavior.)
+    //     e.preventDefault();
 
-        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-        if (!validateEmail(email) || !name) {
-            setErrorMessage('Email or name is invalid');
-            return;
-        }
-        if (!message) {
-            setErrorMessage('Please include a message');
-            return;
-        }
-        // We want to exit out of this code block if something is wrong so that the user can correct it, thus the returns.
+    //     // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    //     if (!validateEmail(email) || !name) {
+    //         setErrorMessage('Email or name is invalid');
+    //         return;
+    //     }
+    //     if (!message) {
+    //         setErrorMessage('Please include a message');
+    //         return;
+    //     }
+    //     // We want to exit out of this code block if something is wrong so that the user can correct it, thus the returns.
 
-        // If successful, we want to clear out the input after registration.
-        setName('');
-        setEmail('');
-        setMessage('');
-        alert(`Hello ${name}`);
-    };
+    //     // If successful, we want to clear out the input after registration.
+    //     setName('');
+    //     setEmail('');
+    //     setMessage('');
+    //     alert(`Hello ${name}`);
+    // };
     // I was able to format the Contact Me form by using Bootstrap columns.  Without it the width of the form elements was super hard to conttrol.
     return (
         <div className="p-2">
@@ -89,7 +95,7 @@ function Form() {
                             />
 
                             <div className="d-grid pt-2">
-                                <button className="btn btn-outline-light" type="button" onClick={handleFormSubmit}>
+                                <button className="btn btn-outline-light" type="button" onClick={clickSubmit}>
                                     Submit
                                 </button>
                             </div>
